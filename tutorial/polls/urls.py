4f8a308beaf6c59f069be4725834f21bc81cceb3
Jorge Rueda <jorgeru045@gmail.com>
2021-02-16 15:16:53 -0500

    Add views and templates

--- tutorial/polls/urls.py
+++ tutorial/polls/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls' # To differentiate URL names between apps
 urlpatterns = [
+    # path(path after polls, view in views.py, name)
     path('', views.index, name='index'),
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
