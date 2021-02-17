bed2ba2b3190a6b61404c2629f5f23ed52d7c2b6
Jorge Rueda <jorgeru045@gmail.com>
2021-02-17 14:48:51 -0500

    Write a minimal form

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,8 +1,9 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 # Create your views here.
-from django.http import HttpResponse
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
@@ -21,11 +22,30 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
+    question = get_object_or_404(Question, pk=question_id)
 
-    return HttpResponse(response % question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        # request.POST is a dictionary-like object that lets you access submitted data by key name.
+        # In this case, request.POST['choice'] returns the ID of the selected choice, as a string.
+        # request.POST values are always strings.
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # reverse function helps avoid having to hardcode a URL in the view function.
+        # It is given the name of the view that we want to pass control to and
+        # the variable portion of the URL pattern that points to that view.
